6cf0ba193a2184b91d4431b0b2ee89d73dcc84e5
鄭丁榮 <jaron.cheng@me.com>
2018-09-08 15:40:03 +0800

    CTJ1809081539 Introduce class "MyStock"

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import stock.get_stock as sgs
 
 #sgs.import_data()
 #sgs.export_data(2014, 2018, '2330')
-df = sgs.import_data('2330', '20170101', '20180816')
+stock_2330 = sgs.MyStock('2330')
+df = stock_2330.import_data('20170101', '20180816')
 print(df)
